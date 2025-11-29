af5c126b27aa11764445ba5379a81f200faf0cad
Javad Komijani <jkomijani@gmail.com>
2025-11-29 12:29:36 +0100

    __init__ files updated for the last three commits.

--- src/lattice_ml/diffusion/__init__.py
+++ src/lattice_ml/diffusion/__init__.py
@@ -1,3 +1,5 @@
 from . import scalar
 from . import gauge
 from ._trainer import *
+from ._noise_schedule import *
+from ._lie_diffusion_process import *
--- src/lattice_ml/diffusion/gauge/__init__.py
+++ src/lattice_ml/diffusion/gauge/__init__.py
@@ -1,3 +1,2 @@
-from ._lie_diffusion_process import SUnDiffusionProcess
 from ._lie_diffusion_model import *
 from ._lie_denoising_process import *
