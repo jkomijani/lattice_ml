0031f71ecae5e2173ee22f4ab3e1f9b419595f93
Javad Komijani <jkomijani@gmail.com>
2026-04-23 22:27:46 +0200

    add backend option

--- src/lattice_ml/linalg/_eig/__init__.py
+++ src/lattice_ml/linalg/_eig/__init__.py
@@ -11,21 +11,47 @@ from .eigsu_3x3 import eigvalssu3x3
 from .eigh_jacobi import jacobi_diagonalization
 
 
-def eigh(matrix, **kwargs):
-    match matrix.shape[-1]:
-        case 2:
-            return eigh2x2(matrix, **kwargs)
-        case 3:
-            return eigh3x3(matrix, **kwargs)
-        case _:
-            return torch.linalg.eigh(matrix, **kwargs)
+def eigh(matrix: torch.Tensor, backend: str = "custom", **kwargs):
+    """
+    Compute the eigendecomposition of the Hermitian input matrix.
+
+    Args:
+        matrix (torch.Tensor): Hermitian matrix of shape (..., n, n).
+        backend (str, optional): Which implementation to use.
+
+    Returns:
+        torch.Tensor: tensor of eigenvalues.
+        torch.Tensor: tensor of eigenvectors.
+    """
+    n = matrix.shape[-1]
+
+    if backend == "custom" and n == 2:
+        return eigh2x2(matrix, **kwargs)
+    if backend == "custom" and n == 3:
+        return eigh3x3(matrix, **kwargs)
+
+    # else use eigh
+    return torch.linalg.eigh(matrix, **kwargs)
 
 
-def eigu(matrix, **kwargs):
-    match matrix.shape[-1]:
-        case 2:
-            return eigu2x2(matrix, **kwargs)
-        case 3:
-            return eign3x3(matrix, **kwargs)  # using eign3x3
-        case _:
-            return torch.linalg.eig(matrix, **kwargs)
+def eigu(matrix: torch.Tensor, backend: str = "custom", **kwargs):
+    """
+    Compute the eigendecomposition of the Unitary input matrix.
+
+    Args:
+        matrix (torch.Tensor): Unitary matrix of shape (..., n, n).
+        backend (str, optional): Which implementation to use.
+
+    Returns:
+        torch.Tensor: tensor of eigenvalues.
+        torch.Tensor: tensor of eigenvectors.
+    """
+    n = matrix.shape[-1]
+
+    if backend == "custom" and n == 2:
+        return eigu2x2(matrix, **kwargs)
+    if backend == "custom" and n == 3:
+        return eign3x3(matrix, **kwargs)  # use eign3x3, more precise
+
+    # else use eigh
+    return torch.linalg.eig(matrix, **kwargs)
