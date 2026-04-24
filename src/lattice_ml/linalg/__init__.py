35f76dfa2b9d411c86217147f8dd54a02fdb290d
Javad Komijani <jkomijani@gmail.com>
2026-04-24 12:09:23 +0200

    add project_data_and_grad_sun

--- src/lattice_ml/linalg/__init__.py
+++ src/lattice_ml/linalg/__init__.py
@@ -9,4 +9,4 @@ from ._autograd import svd
 from ._autograd import svd_with_simplified_ad
 from ._autograd import reciprocal
 
-from ._autograd import project_grad_sun
+from ._autograd import project_grad_sun, project_data_and_grad_sun
