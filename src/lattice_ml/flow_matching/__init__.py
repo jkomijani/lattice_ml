e90a0ad3439efb1c785ba9e732bc3ac1b39047e3
Javad Komijani <jkomijani@gmail.com>
2026-06-12 21:30:00 +0200

    add utils.data subpackage; move _dataset there

--- src/lattice_ml/flow_matching/__init__.py
+++ src/lattice_ml/flow_matching/__init__.py
@@ -2,5 +2,4 @@
 from ._trainer import Trainer  # comment: use FlowMatchingModel
 from ._lie_trainer import LieTrainer  # comment: use SUnFlowMatchingModel
 
-from ._dataset import *
 from ._flow_matching import *
