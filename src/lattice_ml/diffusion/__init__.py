925e3f338de0b18d4fa88deb61619a4925eb0acc
Javad Komijani <jkomijani@gmail.com>
2026-07-07 23:09:10 +0200

    does not import from .scalar (to do: remove scalar folder)

--- src/lattice_ml/diffusion/__init__.py
+++ src/lattice_ml/diffusion/__init__.py
@@ -1,7 +1,8 @@
-from . import scalar
 from . import gauge
+
 from ._diffusion_model import *
+from ._lie_diffusion_process import *
+
 from ._trainer import *
 from ._noise_schedule import *
 from ._sde_schedule import *
-from ._lie_diffusion_process import *
