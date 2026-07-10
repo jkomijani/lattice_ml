2d1d29fd6c6e53a0013fa44799b03ff35f9cce89
Javad Komijani <jkomijani@gmail.com>
2026-07-10 14:44:58 +0200

    organize

--- src/lattice_ml/diffusion/__init__.py
+++ src/lattice_ml/diffusion/__init__.py
@@ -3,6 +3,7 @@ from . import gauge
 from ._diffusion_model import *
 from ._lie_diffusion_process import *
 
-from ._trainer import *
 from ._noise_schedule import *
 from ._sde_schedule import *
+
+from ._trainer import *
